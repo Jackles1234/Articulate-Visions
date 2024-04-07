dd2b328567a831aa3323934c6dd0babf933fbc6a
Cbrown <cooper.brown@drake.edu>
2024-04-07 17:12:18 -0500

    glide-text2im

--- train_utils.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-from main import beta2, beta1, timesteps, device, ab_t
-
-
-def perturb_input(x, t, noise):
-    return ab_t.sqrt()[t, None, None, None] * x + (1 - ab_t[t, None, None, None]) * noise
